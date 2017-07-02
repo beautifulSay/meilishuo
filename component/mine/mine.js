34133338c2b8f2ce94e1a9d048f7ce7e3804bf7f
adu <137154053@qq.com>
2017-07-02 22:39:15 +0800

    222

--- component/mine/mine.js
+++ component/mine/mine.js
@@ -1,4 +1,4 @@
-angular.module('mineModule',['ui.router'])
+angular.module('mineModule',['ui.router',"babyModule"])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('mine', {
